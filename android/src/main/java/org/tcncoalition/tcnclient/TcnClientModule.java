751d190d1228deb30959132fa62e0bd9bf080b00
Nurul Ferdous <nurul@ferdo.us>
2020-05-18 13:19:49 +0600

    first import

---
+++ android/src/main/java/org/tcncoalition/tcnclient/TcnClientModule.java
@@ -0,0 +1,27 @@
+package org.tcncoalition.tcnclient;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class TcnClientModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public TcnClientModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "TcnClient";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        // TODO: Implement some actually useful functionality
+        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+}
